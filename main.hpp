47bf053719db77cdc67bf6bc0de1faec9268631e
Maarij Khan <151799524+Maarij12345@users.noreply.github.com>
2024-11-01 23:38:15 -0400

    main.hpp

---
+++ main.hpp
@@ -0,0 +1,18 @@
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define TABLE_CAPACITY 2    // Capacity for the shared "table" buffer
+#define TOTAL_PRODUCTIONS 10 // Total items to produce and consume
+
+// Structure to represent shared memory with mutex and semaphores
+struct SharedBuffer {
+    pthread_mutex_t lock;
+    sem_t slotsAvailable, itemsAvailable;
+    char table[TABLE_CAPACITY];
+};
