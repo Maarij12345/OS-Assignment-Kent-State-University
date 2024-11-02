2efff982417bf072749a453333440e76d741e453
Maarij Khan <151799524+Maarij12345@users.noreply.github.com>
2024-11-01 23:52:23 -0400

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,11 +1,11 @@
+#include <iostream>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <semaphore.h>
 #include <pthread.h>
 #include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <cstdlib>
 
 #define TABLE_CAPACITY 2    // Capacity for the shared "table" buffer
 #define TOTAL_PRODUCTIONS 10 // Total items to produce and consume
@@ -16,3 +16,4 @@ struct SharedBuffer {
     sem_t slotsAvailable, itemsAvailable;
     char table[TABLE_CAPACITY];
 };
+
