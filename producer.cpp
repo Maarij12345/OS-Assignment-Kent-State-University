9e8f1401211636166b385510d9e119bdf650d912
Maarij Khan <151799524+Maarij12345@users.noreply.github.com>
2024-11-01 23:52:53 -0400

    producer.cpp

--- producer.cpp
+++ producer.cpp
@@ -4,20 +4,20 @@ int main() {
     // Create shared memory object
     int sharedMemory = shm_open("/shared_mem_buffer", O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
     if (sharedMemory == -1) {
-        perror("Producer: Error creating shared memory");
+        std::cerr << "Producer: Error creating shared memory" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-  
-    ftruncate(sharedMemory, sizeof(struct SharedBuffer));
-    struct SharedBuffer *buffer = (struct SharedBuffer *)mmap(NULL, sizeof(*buffer), PROT_READ | PROT_WRITE, MAP_SHARED, sharedMemory, 0);
+    // Configure the size of the shared memory object and map it to `SharedBuffer`
+    ftruncate(sharedMemory, sizeof(SharedBuffer));
+    auto *buffer = static_cast<SharedBuffer *>(mmap(nullptr, sizeof(*buffer), PROT_READ | PROT_WRITE, MAP_SHARED, sharedMemory, 0));
     if (buffer == MAP_FAILED) {
-        perror("Producer: mmap failed");
+        std::cerr << "Producer: mmap failed" << std::endl;
         exit(EXIT_FAILURE);
     }
 
     // Initialize the mutex and semaphores
-    pthread_mutex_init(&buffer->lock, NULL);
+    pthread_mutex_init(&buffer->lock, nullptr);
     sem_init(&buffer->slotsAvailable, 1, TABLE_CAPACITY); // Initially, all slots are available
     sem_init(&buffer->itemsAvailable, 1, 0);              // Initially, no items are available
 
@@ -29,10 +29,10 @@ int main() {
     while (count < TOTAL_PRODUCTIONS) {
         sem_wait(&buffer->slotsAvailable);  // Waits if no empty slot is available
 
-        pthread_mutex_lock(&buffer->lock);  // Lock the bufer for exclusive access
+        pthread_mutex_lock(&buffer->lock);  // Lock the buffer for exclusive access
         buffer->table[position] = item;
-        printf("Producer placed item in position %d: %c\n", position, item);
-        position = (position + 1) % TABLE_CAPACITY;  // Move  the next position in a circular way
+        std::cout << "Producer placed item in position " << position << ": " << item << std::endl;
+        position = (position + 1) % TABLE_CAPACITY;  // Move to the next position in a circular way
         pthread_mutex_unlock(&buffer->lock);
 
         sem_post(&buffer->itemsAvailable);  // Signal that a new item is available
@@ -41,9 +41,10 @@ int main() {
         count++;
     }
 
-    // Cleans up
+    // Clean up
     munmap(buffer, sizeof(*buffer));
     close(sharedMemory);
     shm_unlink("/shared_mem_buffer");
-    exit(EXIT_SUCCESS);
+    return 0;
 }
+
